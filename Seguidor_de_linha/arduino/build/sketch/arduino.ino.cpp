16c46be03ea8f5d01c9f1d381b96d436cb8eadac
Cafta <37232557+Cafta@users.noreply.github.com>
2025-04-18 15:28:39 -0300

    Incluindo a base do projeto para implementação no Arduino

---
+++ Seguidor_de_linha/arduino/build/sketch/arduino.ino.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#line 1 "C:\\GitHub\\PUC\\Seguidor_de_linha\\arduino\\arduino.ino"
+#line 1 "C:\\GitHub\\PUC\\Seguidor_de_linha\\arduino\\arduino.ino"
+void setup();
+#line 5 "C:\\GitHub\\PUC\\Seguidor_de_linha\\arduino\\arduino.ino"
+void loop();
+#line 1 "C:\\GitHub\\PUC\\Seguidor_de_linha\\arduino\\arduino.ino"
+void setup() {
+    pinMode(13, OUTPUT);
+  }
+  
+  void loop() {
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    delay(1000);
+  }
+
